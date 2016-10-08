a2fc4622fa759f4bf8f512f52eccbe333d90b390
sjoerdadema <sjoerd@kristalsoftware.nl>
2016-10-08 15:08:48 +0200

    card component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Card} from "./components/card/card.data";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  cards: Array<Card> = new Array<Card>();
+
+  constructor() {
+    let card: Card = new Card();
+    card.title = "Poelindeling";
+    card.description = "Some quick example text to build on the card title and make up the bulk of the card's content.";
+    this.cards.push(card);
+
+    card = new Card();
+    card.title = "Opening toernooi";
+    card.description = "Some quick example text to build on the card title and make up the bulk of the card's content.";
+    this.cards.push(card);
+  }
 }
---
+++ src/app/components/card/card.data.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by sjoerdadema on 07/10/16.
+ */
+
+export class Card {
+  public title: string = '';
+  public description: string = "";
+
+  constructor() {}
+}
